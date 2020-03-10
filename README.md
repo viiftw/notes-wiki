872b9e58b565aa2041c82a1b5777d5b6e7ad004b
viiftw <datdn.it@gmail.com>
2020-03-10 13:21:04 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# notes-wiki
+
+[![License](https://img.shields.io/badge/license-MIT-_red.svg)](https://opensource.org/licenses/MIT)
+[![Go Report Card](https://goreportcard.com/badge/github.com/viiftw/notes-wiki)](https://goreportcard.com/report/github.com/viiftw/notes-wiki)
+
+Simple web app written in Golang save my notes
+
+## Usage
+
+Clone this repo and go to that folder:
+
+```bash
+go run main.go
+```
+
+Default service run on port 8686
+[http://localhost:8686](http://localhost:8686)
+
+## Feature
+
+- All notes are saved as .txt file in the "notes" folder
+- List all notes
+- Save a note
+- View a note
+- Edit a note
+
+## License
+
+This repo is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).  
+You are welcome to contribute!
--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
-	"fmt"
-	"strings"
 	"path"
+	"regexp"
+	"strings"
 )
 
+// Page data struct for page render
 type Page struct {
 	Title string
 	Body  []byte
 }
 
+// ListPageData list of Page
 type ListPageData struct {
 	PageTitle string
 	Todos     []Page
 }
+
 const baseTemplatePath = "template/"
 const baseNotesPath = "notes/"
 
@@ -69,7 +72,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 var templates = template.Must(template.ParseFiles("template/edit.html", "template/view.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -92,19 +95,19 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 func listHandler(w http.ResponseWriter, r *http.Request) {
 	files, err := ioutil.ReadDir("./notes")
 	if err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	pages := []Page{}
 	for _, file := range files {
-			fn := file.Name()
-			p := Page{Title: strings.TrimSuffix(fn, path.Ext(fn))}
-			pages = append(pages, p)
+		fn := file.Name()
+		p := Page{Title: strings.TrimSuffix(fn, path.Ext(fn))}
+		pages = append(pages, p)
 	}
 
 	data := ListPageData{
 		PageTitle: "My notes list",
-		Todos: pages,
+		Todos:     pages,
 	}
 
 	tmpl := template.Must(template.ParseFiles("template/list.html"))
@@ -115,7 +118,7 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func addHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //get request method
+	// fmt.Println("add method:", r.Method) //get request method
 	if r.Method == "GET" {
 		tmpl := template.Must(template.ParseFiles("template/add.html"))
 		err := tmpl.Execute(w, nil)
@@ -142,6 +145,6 @@ func main() {
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 	http.HandleFunc("/", listHandler)
 	http.HandleFunc("/add", addHandler)
-
+	log.Println("The application is running on port 8686...")
 	log.Fatal(http.ListenAndServe(":8686", nil))
 }
